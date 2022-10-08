4155500e0b5fd6ebbfdfc307c0c4747abc121cf4
iamjoedeveloper <joesuarez.developer@gmail.com>
2022-10-07 20:29:19 -0400

    index.html

---
+++ main.js
@@ -0,0 +1,12 @@
+const menuEmail = document.querySelector('.navbar-email')
+const desktopMenu = document.querySelector('.desktop-menu')
+
+menuEmail.addEventListener('click', toggleDesktopMenu)
+
+function toggleDesktopMenu() {
+	if (desktopMenu.classList.contains('inactive')) {
+		desktopMenu.classList.remove('inactive')
+	} else {
+		desktopMenu.classList.add('inactive')
+	}
+}
