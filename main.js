551314eca2fc9fbb46e1d1fd0fa9157a047a89f2
iamjoedeveloper <joesuarez.developer@gmail.com>
2022-10-10 21:31:03 -0400

    Toggle Menu

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const burgerMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+burgerMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
-	if (desktopMenu.classList.contains('inactive')) {
-		desktopMenu.classList.remove('inactive')
-	} else {
-		desktopMenu.classList.add('inactive')
-	}
+	// if (desktopMenu.classList.contains('inactive')) {
+	// 	desktopMenu.classList.remove('inactive')
+	// } else {
+	// 	desktopMenu.classList.add('inactive')
+	// }
+	desktopMenu.classList.toggle('inactive')
 }
+
+function toggleMobileMenu() {
+	mobileMenu.classList.toggle('inactive')
+}
+
+// Refactorizar el codigo pasando la funcion con ejecución () => toggleMobileMenu(parametro)
