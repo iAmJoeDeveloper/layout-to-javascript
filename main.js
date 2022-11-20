1e9a12ed0963ce1f30456a05c5d60cd4d762ae35
iamjoedeveloper <joesuarez.developer@gmail.com>
2022-11-20 02:18:22 -0400

    Shopping Cart

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const burgerMenu = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 burgerMenu.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu() {
 	// if (desktopMenu.classList.contains('inactive')) {
@@ -12,11 +15,36 @@ function toggleDesktopMenu() {
 	// } else {
 	// 	desktopMenu.classList.add('inactive')
 	// }
+	const isAsideClosed = aside.classList.contains('inactive')
+
+	//If Aside is Open we must to closed it
+	if (!isAsideClosed) {
+		aside.classList.add('inactive')
+	}
+
 	desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu() {
+	const isAsideClosed = aside.classList.contains('inactive')
+
+	//If Aside is Open we must to closed it
+	if (!isAsideClosed) {
+		aside.classList.add('inactive')
+	}
+
 	mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoAside() {
+	const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+
+	//If Mobile Menu is Open we must to closed it
+	if (!isMobileMenuClosed) {
+		mobileMenu.classList.add('inactive')
+	}
+
+	aside.classList.toggle('inactive')
 }
 
 // Refactorizar el codigo pasando la funcion con ejecución () => toggleMobileMenu(parametro)
